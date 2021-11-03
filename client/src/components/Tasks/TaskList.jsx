1e5599c418c3bf162e4ede925180fd24da8ac904
gab-boff <gmeninboff@gmail.com>
2021-11-03 13:52:01 -0300

    Create Form and Tasks Components

---
+++ client/src/components/Tasks/TaskList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Task from "./Task/Task";
+
+const TaskList = () => {
+  return (
+    <>
+      <h1>Task List</h1>
+      <Task />
+      <Task />
+    </>
+  );
+};
+
+export default TaskList;
